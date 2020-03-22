64667c1d6d3d09f6d466c80d5c99e1d3f40ae0cf
LukaszSiadkowski <silukasz@amazon.pl>
2020-03-22 22:01:02 +0100

    two sliders

---
+++ suwak.js
@@ -0,0 +1,7 @@
+function updateTextInput(val) {
+    document.getElementById('textInput').value=val; 
+  }
+
+  function updateTextInput2(val) {
+    document.getElementById('textInput2').value=val; 
+  }
