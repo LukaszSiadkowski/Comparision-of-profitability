283b417b78760eaa9647b91d44e1bd6c63ddba7b
LukaszSiadkowski <silukasz@amazon.pl>
2020-03-30 21:10:07 +0200

    new code version

--- suwak.js
+++ suwak.js
@@ -1,7 +1,63 @@
-function updateTextInput(val) {
-    document.getElementById('textInput').value=val; 
-  }
 
-  function updateTextInput2(val) {
-    document.getElementById('textInput2').value=val; 
-  }
+
+
+
+
+
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const iloscKm = document.getElementById("textPrzebiegu");
+
+// const textPrzebiegu = document.getElementById("przebieg");
+// const textInInput2 = document.getElementById("textInput2");
+
+
+// function updateTextInput(val) {
+//     document.getElementById('textInput').value=val; 
+//   }
+
+//   function updateTextInput2(val) {
+//       let tmpText
+
+//       textInInput2.value=val;
+//     // document.getElementById('textInput2').value=val; 
+//     if (textInInput2.value > 1) {tmpText = `Samochod przejechal`}
+
+//     textPrzebiegu.innerHTML = tmpText;
+// }
+
+  
+
+
+ 
